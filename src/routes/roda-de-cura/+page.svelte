00c1b758969c4d565603e9b1f134a620cd4cd8a6
ViniciusIth <vini.ithalo123@gmail.com>
2023-03-27 17:25:15 -0300

    Added roda de cura page

---
+++ src/routes/roda-de-cura/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<section class="sobre">
+	<div class="container">
+		<h1>Roda de Cura</h1>
+		<p />
+	</div>
+</section>
+<section class="map">
+	<iframe
+		title="Localização do Ponto da Esperança Divina"
+		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2141.0230424107176!2d-48.07882521775367!3d-15.999565326200699!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a2ab485856e0b%3A0xd1ce551d4a57c385!2sPonto%20da%20Esperan%C3%A7a%20Divina!5e0!3m2!1spt-BR!2sbr!4v1679800715968!5m2!1spt-BR!2sbr"
+		height="500"
+		style="border:0;"
+		loading="lazy"
+		referrerpolicy="no-referrer-when-downgrade"
+	/>
+</section>
+
+<style>
+	.map iframe {
+		width: 100%;
+	}
+</style>
