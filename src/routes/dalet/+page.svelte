ed7bf3d79b488a6d692b00789bc622dac30aa013
ViniciusIth <vini.ithalo123@gmail.com>
2023-04-09 17:25:56 -0300

    Adicionado Dalet

---
+++ src/routes/dalet/+page.svelte
@@ -0,0 +1,132 @@
+<script>
+	import chakra from '$lib/assets/chakra.webp';
+</script>
+
+<svelte:head>
+	<title>Servos de Luz - Gravações Dalet</title>
+</svelte:head>
+
+<section class="empty" />
+
+<section class="introduction">
+	<div class="container">
+		<img src={chakra} alt="" />
+		<div class="introduction-text">
+			<h1>Gravações DALET</h1>
+			<cite>
+				Vejo muitas pessoas com os caminhos bloqueados para alcançar o sucesso. Isso inclui o lado
+				profissional, emocional, relacionamentos e saúde. Porém <strong class="primary"
+					>tudo está ao nosso alcance.
+				</strong>
+
+				Nascemos com todas as possibilidades em nossa Espiral de energias, que nos acompanha durante
+				toda a nossa vida. Contudo, com as nossas inseguranças e medos, não acessamos todo o nosso
+				potencial.
+			</cite>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<h2>ALINHAMENTO E COCRIAÇÃO DALET</h2>
+		<p>
+			DALET é a quarta letra do alfabeto hebraico - 4 lados/porta. <br />
+			É uma ferramenta de crescimento, no autodesenvolvimento e evolução do físico, emocional, mental
+			e espiritual. Uma ferramenta que auxilia na expansão da consciência para uma jornada produtiva
+			em todas os aspectos na vida. Através de 4 ondas:
+		</p>
+		<ul>
+			<li>frequência/ressonância</li>
+			<li>Oração</li>
+			<li>Proteção com o Cubo de Metatron</li>
+			<li>simbologia/arquétipos</li>
+		</ul>
+		<p>
+			O Processo foi criado através de estudos e ensinamentos espirituais com seres de outra
+			dimensão, além de estudos da bíblia, cabala, física quântica, registros akáshicos, entre
+			outros. Utilizamos como ferramenta uma gravação personalizada, onde a pessoa iniciará o
+			processo por meio de ondas para uma série de coisas como limpar as toxinas do corpo, energias
+			negativas e crenças limitantes.
+		</p>
+		<p>
+			Com isto teremos resultados emocionais - na confiança, segurança e autoestima - e mentais -
+			concentração, equilíbrio, harmonia e memoria. Além de espirituais com a conexão, o terceiro
+			olho, a intuição, a proteção e o despertar...
+		</p>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<h2>Encontre o sucesso</h2>
+		<p>
+			Muitos de nós queremos mudanças em nossas vidas, mas não queremos pagar o preço. Temos medo de
+			sair da zona de conforto, temos medo de passar pelo processo de purificação. Porque não
+			acreditamos, ou não entendemos, que precisamos fazer nossa parte para alcançar o sucesso. Nos
+			contentamos com soluções paliativas. <br />
+
+			Jesus dizia: "Ajuda-te que o céu te ajudará". Portanto, não espere pelo céu se abrir, abra
+			você mesmo. <br />
+
+			A porta DALET possui a maçaneta. Faça a sua parte e faça-a girar.
+		</p>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<h2>Como Comprar</h2>
+		<p>
+			Após fazer o pagamento, mandar o comprovante com os pedidos que você deseja alcançar que
+			esteja na sua gravação para o email: <a href="mailto:servosdeluzdalet@gmail.com"
+				>servosdeluzdalet@gmail.com</a
+			>. Após o recebimento, responderemos o e-mail em até 10 dias (tempo para ser realizado a
+			gravação que será personalizada) com a gravação. Em caso de dúvida entrar em contato por
+			mensagem nos whatsapp <a href="tel:+55 (86) 99979-3085">(86) 99979-3085</a> 🌀💙
+		</p>
+		<div class="buying-bttns">
+			<a
+				href="https://www.instagram.com/ressonantesdalet"
+				target="_blank"
+				rel="noopener noreferrer"
+				role="button">Saiba Mais</a
+			>
+			<a href="https://mpago.la/1bwMbwL" target="_blank" rel="noopener noreferrer" role="button"
+				>Compre Agora</a
+			>
+		</div>
+	</div>
+</section>
+
+<style>
+	h2 {
+		margin-bottom: 1rem;
+	}
+
+	.introduction div {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 2rem;
+	}
+
+	@media (max-width: 768px) {
+		.introduction div {
+			grid-template-columns: 1fr;
+		}
+	}
+
+	.introduction div img {
+		border-radius: var(--border-radius);
+	}
+
+	.introduction div .introduction-text {
+		display: block;
+	}
+
+	.buying-bttns {
+		display: flex;
+		justify-content: center;
+		gap: 1rem;
+	}
+</style>
