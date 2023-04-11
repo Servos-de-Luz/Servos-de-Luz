7462f7b8d51ba966cdff9a5d4b2ccf25b0616aad
ViniciusIth <vini.ithalo123@gmail.com>
2023-04-11 10:20:25 -0300

    Added vercel configs

--- src/routes/roda-de-cura/+page.svelte
+++ src/routes/roda-de-cura/+page.svelte
@@ -30,9 +30,10 @@
 
 <section class="map">
 	<iframe
-		title="Localização do Ponto da Esperança Divina"
-		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2141.0230424107176!2d-48.07882521775367!3d-15.999565326200699!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a2ab485856e0b%3A0xd1ce551d4a57c385!2sPonto%20da%20Esperan%C3%A7a%20Divina!5e0!3m2!1spt-BR!2sbr!4v1679800715968!5m2!1spt-BR!2sbr"
+		title="Casa Servos de Luz"
+		src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1917.5992495590158!2d-48.0877041661549!3d-16.003179099999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935bd52075679ced%3A0x7dd1e388eb676ec2!2sCasa%20Servos%20de%20Luz!5e0!3m2!1spt-BR!2sbr!4v1681218930718!5m2!1spt-BR!2sbr"
 		height="300"
+		allow="fullscreen"
 		style="border:0;"
 		loading="lazy"
 		referrerpolicy="no-referrer-when-downgrade"
