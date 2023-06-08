aa026822ee3d4e5dad381a54f630b52ae64fa536
ViniciusIth <vini.ithalo@hotmail.com>
2023-06-08 19:27:33 -0300

    Updated project code in general

--- svelte.config.js
+++ svelte.config.js
@@ -10,10 +10,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: true,
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
 		})
 	}
 };
