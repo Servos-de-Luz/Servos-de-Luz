c9ca4a20b1fec5a009ad8d47d54bbd84351ce2b0
ViniciusIth <vini.ithalo@hotmail.com>
2023-06-08 19:28:10 -0300

    Added rosas-de-cura page

--- src/routes/rosas-sagradas/+page.svelte
+++ src/routes/rosas-sagradas/+page.svelte
@@ -1,4 +1,69 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
 </script>
+
+<section class="empty" />
+
+<section>
+	<div class="container">
+		<p>
+			O Caminho das Rosas é a simbologia da nossa caminhada espiritual na Terra rumo a Evolução!
+			Caminhada esta que muitas vezes está cheia de espinhos, os obstáculos! <br />
+			Estamos aqui para atravessar todas as tempestades da nossa alma, buscando e desenvolvendo o nosso
+			desabrochar, o nosso despertar… alcançando o nosso resplendor no qual nos transformará em lindas
+			flores, Rosas majestosas no reino do nosso Mestre Yeshua 🌹
+		</p>
+	</div>
+
+	<div class="container">
+		<p>
+			Em nossa essência mais pura, retornaremos ao nosso lar Celestial. Em união ao todo, nos braços
+			do nosso Pai Amado e Mãe Amada🌹 No Oceano Cósmico do amor ✨💫
+		</p>
+	</div>
+
+	<div class="container">
+		<p>
+			Por que o Caminhos das Rosas? <br />
+			Buscamos conhecimento e fé através das Deusas de todos os tempos. Seres Celestiais que nos auxiliam
+			com a alegria e plenitude como o perfume das Rosas, porém sendo fortes como os espinhos! Uma rosa
+			com espinhos simboliza que a imperfeição faz parte da perfeição, o que envolve nosso aprendizado!
+		</p>
+	</div>
+
+	<div class="container"><p>Caminho das Rosas também presente na logo do @servosdeluz 💙</p></div>
+
+	<div class="container">
+		<p>
+			A Rosa Mística nos faz lembrar quem somos e qual nosso propósito aqui na Terra. Além da
+			beleza, as rosas vibram em 320 Hertz, uma das flores com o maior padrão vibracional de seu
+			reino. Eleva a frequência, nos traz memórias da nossa comunhão com Deus, conhecimento do
+			propósito espiritual de todas(os) nós.
+		</p>
+	</div>
+
+	<div class="container">
+		<p>
+			Nossa Missão: Somos uma irmandade fraternal das @rosas_sagradas em busca da nossa essência,
+			buscando nossa cura, para assim sermos instrumentos de amor e luz na caminhada do próximo. A
+			fazer de cada momento de nossa vida e missão uma oração. Pois tudo o que fazemos é consagrado
+			à Deus Pai e a Deusa Mãe 🌹🙏🏻
+		</p>
+	</div>
+
+	<div class="container">
+		<p>
+			O tripé de sustentação da nossa caminhada é <br />
+			📚Conhecimento (pelo discipulado) <br />
+			🧎🏻‍♀fé (pela adoração) <br />
+			🧘🏻‍♀Contemplação (pela comunhão)
+		</p>
+	</div>
+</section>
+
+<style>
+	@media (max-width: 900px) {
+		section {
+			padding-inline: 25px;
+		}
+	}
+</style>
